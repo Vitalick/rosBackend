d0c7c89fc99e6e610a8468cbf63a0790d482d54b
Vitaly <vitaly.neumoin@gmail.com>
2022-11-02 00:02:49 +0300

    finish ros listener

--- ros_listener.py
+++ ros_listener.py
@@ -2,8 +2,7 @@ import asyncio
 
 import rclpy
 from rclpy.node import Node
-
-from tutorial_interfaces.msg import Num
+from std_msgs.msg import String
 
 from ws_manager import ConnectionManager
 
@@ -13,7 +12,7 @@ class MinimalSubscriber(Node):
     def __init__(self, manager: ConnectionManager):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            Num,
+            String,
             'topic',
             self.listener_callback,
             10)
@@ -21,18 +20,25 @@ class MinimalSubscriber(Node):
         # self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        asyncio.create_task(self.ws_manager.broadcast(f"Message from ros: {msg}"))
+        asyncio.create_task(self.ws_manager.broadcast(f"Message from ros: {msg.data}"))
 
 
 async def ros_listen(manager: ConnectionManager):
     rclpy.init()
 
-    minimal_subscriber = MinimalSubscriber(manager)
-
-    rclpy.spin(minimal_subscriber)
+    while True:
+        minimal_subscriber = MinimalSubscriber(manager)
+        try:
+            while rclpy.ok():
+                rclpy.spin_once(minimal_subscriber, timeout_sec=0.01)
+                await asyncio.sleep(0.01)
+        except Exception as e:
+            print(e)
+            minimal_subscriber.destroy_node()
+        await asyncio.sleep(1)
+    # Get response
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
     rclpy.shutdown()
